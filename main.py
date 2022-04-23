8cbbefe1533986a3f10be393aea295461794f72e
Krushivardhan Reddy <65389673+krushiuniquestar@users.noreply.github.com>
2022-04-23 16:13:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 import configmodel as cm
 import os
 
-cwd = os.getcwd()
-searchConfigPath = os.path.join(cwd, 'Output', 'searches')
-configPath = os.path.join(cwd, 'Output')
-dataModelConfigPath = os.path.join(cwd, 'Output', 'data-model-config')
-
-if os.path.exists(searchConfigPath):
-    pass
-else:
-    os.makedirs(searchConfigPath)
-
-if os.path.exists(dataModelConfigPath):
-    pass
-else:
-    os.makedirs(dataModelConfigPath)
-
-
-def createConfigFiles():
-    files = [cm.searchesConfig(searchConfigPath), cm.config(configPath), cm.dataModelConfig(dataModelConfigPath),
-             cm.configProperties(configPath), cm.xformHTML(searchConfigPath)]
-
-    cm.zipFiles(files, configPath)
-
 
 if __name__ == '__main__':
-    createConfigFiles()
+    
+    cwd = os.getcwd()
+    searchConfigPath = os.path.join(cwd, 'Output', 'searches')
+    configPath = os.path.join(cwd, 'Output')
+    dataModelConfigPath = os.path.join(cwd, 'Output', 'data-model-config')
+
+    if os.path.exists(searchConfigPath):
+        pass
+    else:
+        os.makedirs(searchConfigPath)
+
+    if os.path.exists(dataModelConfigPath):
+        pass
+    else:
+        os.makedirs(dataModelConfigPath)
+
+
+    files = [cm.searchesConfig(searchConfigPath), cm.config(configPath), cm.dataModelConfig(dataModelConfigPath),
+                 cm.configProperties(configPath), cm.xformHTML(searchConfigPath)]
+
+    cm.zipFiles(files, configPath)
